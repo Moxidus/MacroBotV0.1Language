812c95ae4a9312080eae2b070c51b36710546064
Dominik Kutynsky <dominikkutynsky@gmail.com>
2022-01-10 01:01:29 +0100

    support for assets

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using static MainScript;
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace MacroBotV0._1Language
 {
@@ -15,19 +17,41 @@ namespace MacroBotV0._1Language
 
 
 #if RELEASE
+
             string txt = "";
             string fileName = "<none>";
-            if (args.Length == 2 && args[0] == "-f")
+
+            List<string> assetPaths = new List<string>();
+
+            if (args.Length >= 2 && args[0] == "-f")
             {
                 string path = Path.GetFullPath(args[1]);
 
                 txt = System.IO.File.ReadAllText(args[1]);
                 fileName = $"<{Path.GetFileName(path)}>";
             }
-            else if (args.Length == 2 && args[0] == "-t")
+            else if (args.Length >= 2 && args[0] == "-t")
             {
                 txt = args[1];
             }
+
+            if (args.Length >= 4 && args[2] == "-a")
+            {
+                for (int i = 3; i < args.Length; i++)
+                {
+                    assetPaths.Add(args[i]);
+                }
+            }
+
+            List<Image<Bgr, byte>> assetsImages = new List<Image<Bgr, byte>>();
+
+            assetPaths.ForEach(x => Console.WriteLine(x));
+            assetPaths.ForEach(x => assetsImages.Add(new Image<Bgr, byte>(x)));
+
+
+
+
+
 
             if (txt == "")
                 return;
@@ -35,6 +59,8 @@ namespace MacroBotV0._1Language
             txt = txt.Replace(System.Environment.NewLine, "\n");//Raplaces <CR> with new line
 
             Console.WriteLine(txt);
+            //return;
+
             //return;
             (ValueF, CustomError) tokensAndError = MainScript.Run(fileName, txt);
             CustomError error = tokensAndError.Item2;
@@ -45,12 +71,74 @@ namespace MacroBotV0._1Language
             else if (result != null)
                 Console.WriteLine(result.ToString());
 #endif
-            
+
 #if DEBUG
-            for(int i=0; i < args.Length; i++)
+
+            for (int i=0; i < args.Length; i++)
             {
                 Console.WriteLine(args[i]);
             }
+            if(args.Length > 0)
+            {
+
+
+                string txt = "";
+                string fileName = "<none>";
+
+                List<string> assetPaths = new List<string>();
+
+                if (args.Length >= 2 && args[0] == "-f")
+                {
+                    string path = Path.GetFullPath(args[1]);
+
+                    txt = System.IO.File.ReadAllText(args[1]);
+                    fileName = $"<{Path.GetFileName(path)}>";
+                }
+                else if (args.Length >= 2 && args[0] == "-t")
+                {
+                    txt = args[1];
+                }
+
+                if (args.Length >= 4 && args[2] == "-a")
+                {
+                    for (int i = 3; i < args.Length; i++)
+                    {
+                        assetPaths.Add(args[i]);
+                    }
+                }
+
+                List<Image<Bgr, byte>> assetsImages = new List<Image<Bgr, byte>>();
+
+                assetPaths.ForEach(x => Console.WriteLine(x));
+                assetPaths.ForEach(x => assetsImages.Add(new Image<Bgr, byte>(x)));
+
+
+
+
+
+
+                if (txt == "")
+                    return;
+
+                txt = txt.Replace(System.Environment.NewLine, "\n");//Raplaces <CR> with new line
+
+                Console.WriteLine(txt);
+                //return;
+
+                //return;
+                (ValueF, CustomError) tokensAndError = MainScript.Run(fileName, txt);
+                CustomError error = tokensAndError.Item2;
+                ValueF result = tokensAndError.Item1;
+
+                if (error != null)
+                    Console.WriteLine(error);
+                else if (result != null)
+                    Console.WriteLine(result.ToString());
+
+
+            }
+
+            if (args.Length > 0) return;
             while (true) // Basic console simulator
             {
                 Console.Write("MacroScript > "); 
